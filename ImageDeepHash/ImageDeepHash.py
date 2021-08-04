87ec3b214bcb7ca27dcce289528e2aed98a811e2
ffyytt <kiet.duong@opswat.com>
2021-08-04 11:31:44 +0700

    Add basic ImageDeepHash.py

---
+++ ImageDeepHash/ImageDeepHash.py
@@ -0,0 +1,35 @@
+import numpy as np
+import tensorflow as tf
+import matplotlib.pyplot as plt
+
+from ImageDeepHash.utils.utils import *
+from ImageDeepHash.ImageLoader import ImageLoader
+
+
+class ImageDeepHash:
+    def __init__(self, hex_len=16, root="./", image_size=(224, 224, 3)):
+        self.hex_len = hex_len
+        self.root = root
+        self.image_size = image_size
+        self.imageLoader = ImageLoader.ImageLoader(self.root, self.image_size)
+        self.image = np.zeros(self.image_size)
+        self.list_digest = [0]*self.hex_len*8
+
+        self.model = tf.keras.applications.VGG16(include_top=True, weights=None, classes=128,
+                                                 classifier_activation='sigmoid')
+
+    def hash(self, path):
+        self.image = self.imageLoader.load(path)
+        self.list_digest = (self.model.predict(np.array([self.image]))[0] > 0.5).astype(np.int).tolist()
+
+    def digest(self):
+        bin_str = "".join(map(str, self.list_digest))
+        return long_to_bytes(int(bin_str, 2))
+
+    def hexdigest(self):
+        bin_str = "".join(map(str, self.list_digest))
+        return hex(int(bin_str, 2))
+
+    def plot(self):
+        plt.imshow(self.image)
+        plt.show()
--- ImageDeepHash/ImageLoader/ImageLoader.py
+++ ImageDeepHash/ImageLoader/ImageLoader.py
@@ -4,14 +4,16 @@ import numpy as np
 
 
 class ImageLoader:
-    def __init__(self, root="./"):
+    def __init__(self, root="./", image_size=(224, 224, 3)):
         self.root = root
+        self.image_size = image_size
 
     def load(self, filename):
         path = os.path.join(self.root, filename)
 
         image = cv2.imread(path, cv2.IMREAD_COLOR).copy().astype(np.float32)
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB).astype(np.float32)
+        image = cv2.resize(image, (self.image_size[0], self.image_size[1]))
         image /= 255.0
 
         return image
